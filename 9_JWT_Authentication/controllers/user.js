8842d6678d1a20587cd3a25b3eeacfef3175a562
kushal1997 <kushalrao103@gmail.com>
2024-06-01 09:15:16 +0530

    modals and controller are made

---
+++ 9_JWT_Authentication/controllers/user.js
@@ -0,0 +1,12 @@
+const handleUserSignUp=async(req,res)=>{
+    const {name,email,password}=req.body;
+    await User.create({
+        name,email,password
+    })
+
+    return res.render('home')
+}
+
+module.exports={
+    handleUserSignUp
+}
