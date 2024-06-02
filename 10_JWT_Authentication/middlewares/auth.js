e6b522d9386c6e88d368d70cecdf7a8426b9e075
kushal1997 <kushalrao103@gmail.com>
2024-06-02 12:06:29 +0530

    how to pass token in headers and use it

--- 10_JWT_Authentication/middlewares/auth.js
+++ 10_JWT_Authentication/middlewares/auth.js
@@ -2,17 +2,22 @@ const { getUser } = require("../services/auth");
 
 async function restrictToLoggedUserOnly(req,res,next){
     // console.log(req)
-    const userUid=req.cookies?.uid;
-    if(!userUid) return res.redirect('/login');
-    const user=getUser(userUid);
+    const userId=req.headers.authorization;
+    if(!userId) return res.redirect('/login');
+console.log("userId",userId) 
+    const token=userId.split('Bearer ')[1];
+    
+    const user=getUser(token);
     if(!user) return res.redirect('/login');
 
     req.user=user;
     next();
 }
 async function checkAuth(req,res,next){
-    const userUid=req.cookies?.uid;
-    const user=getUser(userUid);
+    const userUid=req.headers.authorization;
+    console.log("userId",userUid,"headers",req.headers) 
+    const token=userUid.split('Bearer ')[1];
+    const user=getUser(token);
     console.log(user)
     req.user=user;
     next();
