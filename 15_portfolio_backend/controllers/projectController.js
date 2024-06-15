aff920b228af35518797fd0c409db641874787a6
kushal1997 <kushalrao103@gmail.com>
2024-06-16 05:15:46 +0530

    added developedAt to short the data in UI

--- 15_portfolio_backend/controllers/projectController.js
+++ 15_portfolio_backend/controllers/projectController.js
@@ -1,7 +1,7 @@
-const Project = require('../models/project');
+const Project = require("../models/project");
 
 exports.createProject = async (req, res) => {
-  const { name, url, githubUrl, description, languages } = req.body;
+  const { name,developedAt, url, githubUrl, description, languages } = req.body;
   const img = req.file.location; // Assuming you are using AWS S3 and `req.file.location` provides the image URL
 
   // Ensure `isFork` is set to a default value or provided in the request body
@@ -10,65 +10,74 @@ exports.createProject = async (req, res) => {
   try {
     const project = new Project({
       name,
+      developedAt,
       img,
       url,
       githubUrl,
       description,
       isFork,
-      languages: JSON.parse(languages)
+      languages: JSON.parse(languages),
     });
 
     await project.save();
-    res.redirect('/');
+    res.redirect("/");
   } catch (err) {
     console.error(err); // Log the error for debugging
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 };
 
 exports.getProjects = async (req, res) => {
   try {
     const projects = await Project.find();
-    res.render('index', { projects });
+    res.render("index", { projects });
   } catch (err) {
     console.error(err); // Log the error for debugging
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 };
 
 // GET /api/projects - Get all projects
 exports.getAllProjects = async (req, res) => {
-    try {
-      const projects = await Project.find();
-      res.json(projects); // Respond with JSON array of projects
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Server Error' });
-    }
-  };
+  try {
+    const projects = await Project.find();
+    res.json(projects); // Respond with JSON array of projects
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
-
-  // POST /api/projects - Create a new project
+// POST /api/projects - Create a new project
 exports.createProjectAPI = async (req, res) => {
-    const { name, createdAt, url, githubUrl, description, languages, isFork } = req.body;
-    const img = req.file.location; // Assuming you are using AWS S3 and `req.file.location` provides the image URL
-  
-    try {
-      const newProject = new Project({
-        name,
-        createdAt,
-        img,
-        url,
-        githubUrl,
-        description,
-        languages: JSON.parse(languages),
-        isFork,
-      });
-  
-      const project = await newProject.save();
-      res.status(201).json(project); // Respond with JSON object of created project
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Server Error' });
-    }
-  };
+  const {
+    name,
+    developedAt,
+    url,
+    githubUrl,
+    description,
+    languages,
+    isFork,
+  } = req.body;
+  console.log(req.body)
+  const img = req.file.location; // Assuming you are using AWS S3 and `req.file.location` provides the image URL
+
+  try {
+    const newProject = new Project({
+      name,
+      developedAt,
+      img,
+      url,
+      githubUrl,
+      description,
+      languages: JSON.parse(languages),
+      isFork,
+    });
+
+    const project = await newProject.save();
+    res.status(201).json(project); // Respond with JSON object of created project
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
--- 15_portfolio_backend/models/project.js
+++ 15_portfolio_backend/models/project.js
@@ -1,18 +1,22 @@
 const mongoose = require("mongoose");
 
-const ProjectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  img: { type: String, required: true },
-  url: { type: String, required: true },
-  githubUrl: { type: String, required: true },
-  description: { type: String, required: true },
-  isFork: { type: Boolean, required: true },
-  languages: [
-    {
-      name: { type: String, required: true },
-      iconifyClass: { type: String, required: true },
-    },
-  ],
-},{timestamps:true});
+const ProjectSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    developedAt: { type: Date, required: true },
+    img: { type: String, required: true },
+    url: { type: String, required: true },
+    githubUrl: { type: String, required: true },
+    description: { type: String, required: true },
+    isFork: { type: Boolean, required: true },
+    languages: [
+      {
+        name: { type: String, required: true },
+        iconifyClass: { type: String, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Project", ProjectSchema);
